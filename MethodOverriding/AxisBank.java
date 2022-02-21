9bbcb380f580d2b730d650d0c1d91909f9c9832c
AjithKumar <99200359+AjithkumarEngineering@users.noreply.github.com>
2022-02-21 19:33:00 +0530

    Add files via upload

---
+++ MethodOverriding/AxisBank.java
@@ -0,0 +1,14 @@
+package MethodOverriding;
+
+public class AxisBank extends BankInfo {
+	public void deposit() {
+		System.out.println("1000");
+	}
+	public static void main(String[] args) {
+		AxisBank r1=new AxisBank();
+		r1.deposit();
+		r1.savings();
+		r1.fixed();
+	}
+
+}
